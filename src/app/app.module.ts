9c1e6fb4a020cadd77b19e0cf3033072003bc20c
Clément Deriencourt <clement-deriencourt@orange.fr>
2020-09-24 12:49:41 +0200

    Table w/ filter for learn values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableFilterComponent } from './table-filter/table-filter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {CdkTable, CdkTableModule, DataSource} from '@angular/cdk/table';
+import {MatColumnDef, MatRow, MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRowHarness} from '@angular/material/table/testing';
+import {MatInput, MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatPaginatorModule,
+    CdkTableModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule
+
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
   ],
   providers: [],
   bootstrap: [AppComponent]
